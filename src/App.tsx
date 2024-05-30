9831d05012d5f04a45ed37c22f9b581f9699f738
Nilesh Kachare <nileshkachare728@gmail.com>
2024-05-30 07:38:33 +0530

    learn about component type parameter

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+import { Button } from './component/component-type-props/component-type-props'
+function App() {
+  
+
+  return (
+    <>
+     <Button className="my-class" />
+
+     <Button />
+    </>
+  )
+}
+
+export default App
---
+++ src/component/component-type-props/component-type-props.tsx
@@ -0,0 +1,9 @@
+
+
+export const Button = (props) =>{
+    return(
+        <button className={props.className}>
+            Click Me
+        </button>
+    )
+}
